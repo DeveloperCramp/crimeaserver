a1099f33e15a3f56d25349d475b2954321d9a8fc
DeveloperCramp <biodubstep@gmail.com>
2022-04-27 23:25:56 +0000

    Add event "OnPlayerConnected" - send chat message Add event "OnPlayerSpawn" - set health 50%, armor 50%

---
+++ crimeaserver/Events.cs
@@ -0,0 +1,20 @@
+﻿using GTANetworkAPI;
+
+namespace crimeaserver
+{
+    internal class Events : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("~b~Hello on ~g~Crimea RP!");
+        }
+
+        [ServerEvent(Event.PlayerSpawn)]
+        private void OnPlayerSpawn(Player player)
+        {
+            player.Health = 50;
+            player.Armor = 50;
+        }
+    }
+}
